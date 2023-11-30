6d82e059c203b532cbcb235727167ff92eb8a46c
mahsa abtahi <mahsajaniii13@gmail.com>
2023-11-30 18:16:41 +0330

    add search icon

--- components/header/index.js
+++ components/header/index.js
@@ -1,9 +1,11 @@
 import Top from "./Top.js";
 import styles from "./styles.module.scss";
+import Main from "./Main";
 export  default function Header() {
     return (
         <div>
             <Top />
+            <Main />
        
         </div>
     );
